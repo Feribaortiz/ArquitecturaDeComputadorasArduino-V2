9fc62479969073b77187d20cd35aebf88cd2ea40
feribaortiz <fernandoibaortiz@gmail.com>
2018-12-11 01:32:58 -0700

    Se modifico el consulta usuarios

--- phpMuseo/consultaUsuario.php
+++ phpMuseo/consultaUsuario.php
@@ -1,20 +1,24 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    
+    $idUsuario = $_POST["idusuario"];
+    $contraseña = $_POST["contra"];    
+
     $servidor="localhost";
     $usuario = "root";
     $password="";
     $basedatos="museo";
     $conexion=mysqli_connect($servidor,$usuario,$password,$basedatos);
-    $resultado = mysqli_query($conexion, "SELECT * FROM usuarios");
+    $consulta=sprintf("SELECT * FROM usuarios WHERE idusuario = %s and contrasena =%s",$idUsuario,$contraseña);
+    $resultado = mysqli_query($conexion, $consulta);
+    $filas = mysqli_num_rows($resultado);
     $salida = array();
-    if(mysqli_num_rows($resultado) > 0){
-        while($registro = mysqli_fetch_array($resultado,MYSQLI_ASSOC)){
-            array_push($salida, $registro);
-            print json_encode($salida);
-        }
-            
+    if(mysqli_num_rows($resultado) >= 0){
+        while($registro = mysqli_fetch_array($resultado)){
+                $nombre=$registro["idusuario"];
+                $ncontrol  = $registro["contrasena"];
+                $usuario =  array('nombre'=> $nombre,'ncontrol' => $ncontrol );
+        }       
     }
-    print json_encode($salida);
-    die();
+    $salidaJSON = array('usuario' => $usuario);
+        print json_encode($salidaJSON);
 ?>
